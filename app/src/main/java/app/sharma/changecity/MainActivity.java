35bf280cbb0146cbc613fab6bfc9282ba301a7b6
Shivam Sharma <kshivam654@gmail.com>
2020-08-23 14:21:30 +0530

    Custom dialog box and location enable setting are 200k

--- app/src/main/java/app/sharma/changecity/MainActivity.java
+++ app/src/main/java/app/sharma/changecity/MainActivity.java
@@ -1,14 +1,173 @@
 package app.sharma.changecity;
+// my google maps api key:  AIzaSyAF0I5ji4MMQ3n1Bp5lQ68Ge7RD74uMfU0
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.IntentSender;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    String apiKey = "AIzaSyAF0I5ji4MMQ3n1Bp5lQ68Ge7RD74uMfU0";
+
+    protected static final String TAG = "LocationOnOff";
+
+    private GoogleApiClient googleApiClient;
+    final static int REQUEST_LOCATION = 199;
+
+    Button button;
+
+    Dialog myDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        myDialog = new Dialog(this);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button txtclose;
+                Button btnChange;
+                TextView curCity;
+
+                myDialog.setContentView(R.layout.custom_popup);
+
+                txtclose = (Button) myDialog.findViewById(R.id.txtclose);
+                btnChange = (Button) myDialog.findViewById(R.id.btnChange);
+                curCity = (TextView) myDialog.findViewById(R.id.currentCity);
+                txtclose.setText("OK");
+                txtclose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        myDialog.dismiss();
+                    }
+                });
+                btnChange.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        // Todo Location Already on  ... start
+                        final LocationManager manager = (LocationManager) MainActivity.this.getSystemService(Context.LOCATION_SERVICE);
+                        if (manager.isProviderEnabled(LocationManager.GPS_PROVIDER) && hasGPSDevice(MainActivity.this)) {
+                            Toast.makeText(MainActivity.this,"Gps already enabled",Toast.LENGTH_SHORT).show();
+                        }
+                        // Todo Location Already on  ... end
+
+                        if(!hasGPSDevice(MainActivity.this)){
+                            Toast.makeText(MainActivity.this,"Gps not Supported",Toast.LENGTH_SHORT).show();
+                        }
+
+                        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER) && hasGPSDevice(MainActivity.this)) {
+                            Log.e("keshav","Gps already enabled");
+                            Toast.makeText(MainActivity.this,"Gps not enabled",Toast.LENGTH_SHORT).show();
+                            enableLoc();
+                        }else{
+                            Log.e("keshav","Gps already enabled");
+                            Toast.makeText(MainActivity.this,"Gps already enabled", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+                myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                myDialog.show();
+            }
+        });
     }
+
+    private boolean hasGPSDevice(Context context) {
+        final LocationManager mgr = (LocationManager) context
+                .getSystemService(Context.LOCATION_SERVICE);
+        if (mgr == null)
+            return false;
+        final List<String> providers = mgr.getAllProviders();
+        if (providers == null)
+            return false;
+        return providers.contains(LocationManager.GPS_PROVIDER);
+    }
+
+    private void enableLoc() {
+
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(MainActivity.this)
+                    .addApi(LocationServices.API)
+                    .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                        @Override
+                        public void onConnected(Bundle bundle) {
+
+                        }
+
+                        @Override
+                        public void onConnectionSuspended(int i) {
+                            googleApiClient.connect();
+                        }
+                    })
+                    .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                        @Override
+                        public void onConnectionFailed(ConnectionResult connectionResult) {
+
+                            Log.d("Location error","Location error " + connectionResult.getErrorCode());
+                        }
+                    }).build();
+            googleApiClient.connect();
+
+            LocationRequest locationRequest = LocationRequest.create();
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            locationRequest.setInterval(30 * 1000);
+            locationRequest.setFastestInterval(5 * 1000);
+            LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                    .addLocationRequest(locationRequest);
+
+            builder.setAlwaysShow(true);
+
+            PendingResult<LocationSettingsResult> result =
+                    LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+            result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+                @Override
+                public void onResult(LocationSettingsResult result) {
+                    final Status status = result.getStatus();
+                    switch (status.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                            try {
+                                // Show the dialog by calling startResolutionForResult(),
+                                // and check the result in onActivityResult().
+                                status.startResolutionForResult(MainActivity.this, REQUEST_LOCATION);
+
+                                Toast.makeText(MainActivity.this, "Shivam its done as of now", Toast.LENGTH_SHORT).show();
+                            } catch (IntentSender.SendIntentException e) {
+                                // Ignore the error.
+                            }
+                            break;
+                    }
+                }
+            });
+        }
+    }
+
 }
